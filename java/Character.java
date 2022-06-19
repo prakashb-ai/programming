85e4e0d7e805bc8173bd3bdd287a4e4fca0b885c
prakashb-ai <prakashbodicse@gmail.com>
2022-06-20 00:12:03 +0530

    add problems in java

--- java/Character.java
+++ java/Character.java
@@ -8,6 +8,7 @@
         System.out.println("enter the string: ");
         str = sc.nextLine();
         for(int i=0;i<str.length();i++){
+            
             char ch = str.charAt(i);
             if(ch>='A' && ch<='Z' || ch>='a' && ch<='z'){
                 Alphabets++;
---
+++ java/Firstlast.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Firstlast{
+    public static void main(String args[]){
+        String first,last;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter a first characher");
+        first = sc.nextLine();
+        char fch = first.charAt(0);
+
+        System.out.println("enter a last character");
+        last = sc.nextLine();
+        char lch= last.charAt(first.length()-1);
+
+        System.out.println("first character is ::"+fch+"last character::"+lch);
+    }
+}
