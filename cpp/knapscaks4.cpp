73fb384d98b64131547fbfde53e853ece62d1dc7
prakashb-ai <prakashbodicse@gmail.com>
2022-06-23 00:32:25 +0530

    .

--- cpp/knapscaks4.cpp
+++ cpp/knapscaks4.cpp
@@ -7,25 +7,21 @@ int max(int a,int b){
         return b;
     }
 }
-
 int knapsack(int W,int wt[],int p[],int n){
     int w,i;
-    int knap[n+1][W+1];
+    int knp[n+1][W+1];
     for(i=0;i<=n;i++){
-        for(w=0;w<=W;w++){
-            if(i == 0 || w == 0)
-             knap[i][w] = 0;
-
-             else if(wt[i-1]<=w)
-              knap[i][w] = max(p[i-1] + knap[i-1][w-wt[i-1]],knap[i-1][w]);
-
-              else 
-                knap[i][w] = knap[i-1][w];
-        }
+         for(w=0;w<=W;w++){
+             if(i==0 || w==0)
+               knp[i][w] =0;
+               else if(wt[i-1]<=w)
+                knp[i][w] = max(p[i-1]+knp[i-1][w-wt[i-1]],knp[i-1][w]);
+                else 
+                 knp[i][w] = knp[i-1][w];
+         }
     }
-    return knap[n][W];
+    return knp[n][W];
 }
-
 int main(){
     int n,W;
     printf("\n enter the number of items:-");
